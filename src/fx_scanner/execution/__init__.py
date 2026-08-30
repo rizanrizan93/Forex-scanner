e9aaec910f88e39b49280ba0312f541e70ad8459
rizanrizan93 <rizanrizan93@gmail.com>
2026-08-30 13:37:27 +0700

    feat: export dual-feed execution components

--- src/fx_scanner/execution/__init__.py
+++ src/fx_scanner/execution/__init__.py
@@ -1,3 +1,5 @@
+from .audit_async import AsyncOperationalAudit
+from .cadence import AdaptiveExecutionCadence, ExecutionWatchState
 from .control_plane import ControlPlaneBlocked, ControlPlaneGate, ControlPlaneRefreshWorker
 from .broker_gateway import (
     BrokerAccountSnapshot,
@@ -7,7 +9,9 @@ from .broker_gateway import (
     BrokerPreflight,
 )
 from .ctrader_gateway import CTraderExecutionGateway, CTraderPreparedOrder
-from .factory import build_broker_gateway
+from .factory import DualBrokerStack, build_broker_gateway, build_dual_broker_stack
+from .reconciliation import DualFeedRevalidator, RevalidationBlocked, RevalidatedOrder, RevalidationMetrics
+from .symbol_mapping import MT5SymbolResolver, ResolvedSymbol
 from .models import ExecutionMode, OrderIntent, OrderReceipt, OrderSide, OrderType
 from .policy import ExecutionPolicy, load_execution_policy
 from .router import ExecutionBlocked, ExecutionRouter
@@ -24,6 +28,17 @@ from .runtime import (
 from .service import RuntimeHandlers, TradingRuntimeService
 
 __all__ = [
+    "AsyncOperationalAudit",
+    "AdaptiveExecutionCadence",
+    "ExecutionWatchState",
+    "DualBrokerStack",
+    "build_dual_broker_stack",
+    "DualFeedRevalidator",
+    "RevalidationBlocked",
+    "RevalidatedOrder",
+    "RevalidationMetrics",
+    "MT5SymbolResolver",
+    "ResolvedSymbol",
     "ControlPlaneBlocked",
     "ControlPlaneGate",
     "ControlPlaneRefreshWorker",
