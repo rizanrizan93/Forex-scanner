384c1558510b868193fe43fc4c9c548a6f09400f
rizanrizan93 <rizanrizan93@gmail.com>
2026-09-02 20:13:53 +0700

    test: update USD coverage for XAUUSD universe

--- tests/test_macro_ranking_v06.py
+++ tests/test_macro_ranking_v06.py
@@ -85,7 +85,7 @@ def test_currency_strength_respects_base_quote_orientation():
     assert strength["EUR"].score == 80
     assert strength["GBP"].score == 40
     assert strength["USD"].contributing_pairs == 3
-    assert strength["USD"].expected_pairs == 7
+    assert strength["USD"].expected_pairs == 8
     assert strength["USD"].coverage == 3 / 7
     assert strength["EUR"].coverage == 1 / 5
     assert strength["USD"].score < 0
