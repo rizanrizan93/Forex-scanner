b5ccd646c33698168d09844e86b081dc6fe94e6b
rizanrizan93 <rizanrizan93@gmail.com>
2026-09-05 09:36:38 +0700

    test(demo): preserve normalized weekend spread caps

--- tests/test_demo_weekend_spread_workflow_v051.py
+++ tests/test_demo_weekend_spread_workflow_v051.py
@@ -10,8 +10,8 @@ def test_weekend_discovery_clears_only_inactive_weekday_spread_overrides():
     assert 'date -u +%u' in workflow
     assert 'export CTRADER_DEMO_XAUUSD_MAX_SPREAD_PIPS=""' in workflow
     assert 'export CTRADER_DEMO_XTIUSD_MAX_SPREAD_PIPS=""' in workflow
-    assert 'CTRADER_DEMO_BTCUSD_MAX_SPREAD_PIPS: "2000"' in workflow
-    assert 'CTRADER_DEMO_ETHUSD_MAX_SPREAD_PIPS: "100"' in workflow
+    assert 'CTRADER_DEMO_BTCUSD_MAX_SPREAD_PIPS: "20"' in workflow
+    assert 'CTRADER_DEMO_ETHUSD_MAX_SPREAD_PIPS: "1"' in workflow
     assert 'CTRADER_DEMO_SOLUSD_MAX_SPREAD_PIPS: "120"' in workflow
     assert 'CTRADER_DEMO_EXECUTION_CANDIDATE_MIN: "50.01"' in workflow
     assert 'CTRADER_DEMO_RISK_PER_TRADE_PCT: "1.0"' in workflow
